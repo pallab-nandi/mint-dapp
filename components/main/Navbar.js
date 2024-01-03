026991bfc84e23b800ca67783271b19456cea5da
Sanjoy Sarkar <sanjoysarkar@outlook.in>
2024-01-03 12:17:22 +0530

    Added :link tag to navbar

--- components/main/Navbar.js
+++ components/main/Navbar.js
@@ -3,12 +3,13 @@ import Image from "next/image";
 import React from "react";
 import WalletConnect from "../sub/WalletConnect";
 import AudioPlayer from "../sub/AudioPlayer";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className="w-full h-[70px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] px-0 md:px-20 backdrop-blur-md z-50 ">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
-        <a href="/" className="h-auto w-auto flex flex-row items-center">
+        <Link href="/" className="h-auto w-auto flex flex-row items-center">
           <Image
             src="/NavLogo.png"
             alt="logo"
@@ -16,11 +17,7 @@ const Navbar = () => {
             height={70}
             className="hover:scale-125"
           />
-
-          {/* <span className="font-bold ml-[10px] hidden md:block text-gray-300">
-            NFA
-          </span> */}
-        </a>
+        </Link>
 
         <div className=" h-full flex flex-row items-center pl-0 md:pl-12 justify-center">
           <WalletConnect />
